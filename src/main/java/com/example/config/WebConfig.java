70ef7928dec2228a8eca8425b20f0134b1696acb
NgocLan <nguyenthingoclan2004@gmail.com>
2025-12-12 19:53:04 +0700

    Thêm viết đánh giá sản phẩm (product_detail)

--- src/main/java/com/example/config/WebConfig.java
+++ src/main/java/com/example/config/WebConfig.java
@@ -4,10 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.example.interceptor.RememberMeInterceptor;
 import com.example.service.RememberMeService;
+
+import java.nio.file.Paths;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -28,5 +31,13 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addInterceptor(rememberMeInterceptor)
                 .addPathPatterns("/**")
                 .excludePathPatterns("/admin/login","/admin/logout","/css/**", "/js/**");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        String projectPath = Paths.get(".").toAbsolutePath().normalize().toString();
+
+        registry.addResourceHandler("/uploads/**")
+                .addResourceLocations("file:/" + projectPath + "/uploads/");
     }
 }
